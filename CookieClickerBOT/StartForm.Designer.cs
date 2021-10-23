d419f8e6be9b4f513575e6bd9f99b6cecc290f0b
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-10-23 19:06:29 +0300

    Add project files.

---
+++ CookieClickerBOT/StartForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CookieClickerBOT
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumTime = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(4, 46);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(178, 23);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start BOT";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bekleme Süresi :";
+            // 
+            // NumTime
+            // 
+            this.NumTime.Location = new System.Drawing.Point(95, 8);
+            this.NumTime.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.NumTime.Name = "NumTime";
+            this.NumTime.Size = new System.Drawing.Size(87, 23);
+            this.NumTime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ms";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CookieClickerBOT.Properties.Resources.cookie;
+            this.pictureBox1.Location = new System.Drawing.Point(188, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 74);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NumTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CookieClicker BOT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StartForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.NumTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NumTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CookieClickerBOT/StartForm.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace CookieClickerBOT
+{
+    public partial class StartForm : Form
+    {
+        IWebDriver WebDriver;
+        IWebElement CookieElement;
+        private bool active = false;
+
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            active = !active;
+            NumTime.Enabled = !active;
+
+            if (WebDriver is null)
+            {
+                //Webdriver Interfaceine Chrome kullanacağımızı belirtiyoruz.
+                WebDriver = ChromeDriverInit();
+                //Web Adresimize Gidiyoruz
+                WebDriver.Navigate().GoToUrl("https://orteil.dashnet.org/cookieclicker/");
+                // Tıklayacağımız nesnemizi seçiyoruz
+                CookieElement = WebDriver.FindElement(By.Id("bigCookie"));
+            }
+
+            // PROGRAMIMIZIN ASENKRON OLARAK TIKLAMA YAPMASINI SAĞLIYORUZ
+            Thread _thread = new Thread(() =>
+            {
+                if (CookieElement != null)
+                {
+                    while (active)
+                    {
+                        try
+                        {
+                            Thread.Sleep((int)NumTime.Value);
+                            CookieElement.Click();
+                        }
+                        catch { }
+               
+                    }
+                }
+            });
+
+            _thread.Start();
+
+
+        }
+
+        public ChromeDriver ChromeDriverInit()
+        {
+            var chromeOptions = new ChromeOptions();
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            return new ChromeDriver(chromeDriverService, chromeOptions);
+        }
+
+        public void CheckProcesses()
+        {
+            if (WebDriver != null)
+            {
+                WebDriver.Dispose();
+
+                Process[] killChrome = Process.GetProcessesByName("chromedriver.exe");
+
+                foreach (var process in killChrome)
+                {
+                    process.Kill();
+                }
+
+            }
+        }
+
+        private void StartForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // açık olan driverları dispose ediyoruz.
+            CheckProcesses();
+        
+        }
+    }
+}
